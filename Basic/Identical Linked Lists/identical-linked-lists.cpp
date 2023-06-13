d03e49dae7d352cfe3cca646beb0c8e44be0ce2f
Naman Hiran <106793339+NamanH16@users.noreply.github.com>
2023-06-13 12:13:13 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Identical Linked Lists/identical-linked-lists.cpp
@@ -0,0 +1,94 @@
+//{ Driver Code Starts
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+bool areIdentical(struct Node *a, struct Node *b);
+
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--){
+        int n1, n2, tmp , d1 , d2;
+        struct Node *head1 = NULL , *tail1=NULL;
+        struct Node *head2 = NULL , *tail2 =NULL;
+        cin>>n1;
+        cin>>d1;
+        head1 = new Node(d1);
+        tail1 = head1;
+        while(n1-- > 1){
+            cin>>tmp;
+            tail1->next = new Node(tmp);
+            tail1 = tail1->next;
+        }
+        cin>>n2;
+        cin>>d2;
+        head2 = new Node(d2);
+        tail2 = head2;
+        while(n2-- >1)
+        {
+            cin>>tmp;
+            tail2->next = new Node(tmp);
+            tail2 = tail2->next;
+        }
+        areIdentical(head1, head2)?cout<<"Identical"<<endl:cout<<"Not identical"<<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/*
+Structure of the node of the linked list is as
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+//Function to check whether two linked lists are identical or not. 
+int len(struct Node *t){
+    int l=0;
+    while(t!=NULL){
+        t=t->next;
+        l++;
+    }
+    return l;
+}
+bool areIdentical(struct Node *head1, struct Node *head2)
+{
+    // Code here
+    struct Node *temp1 = head1;
+    struct Node *temp2 = head2;
+    if(temp1==NULL && temp2==NULL)return true;
+    if((temp1==NULL && temp2!=NULL) || (temp1!=NULL && temp2==NULL))return false;
+    if(len(temp1)!=len(temp2)){
+        return false;
+    }
+    while(temp1!=NULL && temp2!=NULL){
+        if(temp1->data != temp2->data){
+            return false;
+        }
+        temp1=temp1->next;
+        temp2=temp2->next;
+    }
+    return true;
+}
