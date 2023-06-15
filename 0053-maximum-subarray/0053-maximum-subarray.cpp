eb45921f5ff4f39350fb89fe151b9bc2bb2cf565
Naman Hiran <106793339+NamanH16@users.noreply.github.com>
2023-06-15 16:08:56 +0530

    Time: 116 ms (79.63%), Space: 67.7 MB (86.39%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int n = nums.size();
-        int curr_sum=0, best_so_far=INT_MIN;
+        int curr_sum=0, best=INT_MIN;
         for(int i=0;i<n;i++){
             curr_sum+=nums[i];
-            if(curr_sum>best_so_far){
-                best_so_far=curr_sum;
+            if(curr_sum>best){
+                best=curr_sum;
             }
             if(curr_sum<0){
                 curr_sum=0;
             }
         }
-        return best_so_far;
+        return best;
     }
 };
