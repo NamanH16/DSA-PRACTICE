049e0739473d734f40be9873064191ac58ddcabb
Naman Hiran <106793339+NamanH16@users.noreply.github.com>
2023-06-19 19:52:35 +0530

    Time: 8 ms (8.49%), Space: 9.1 MB (44.47%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i=m-1, j=n-1, k = m+n-1;
-        while(i>=0 && j>=0){
-            if(nums1[i]<nums2[j]){
-                nums1[k]=nums2[j];
-                k--;j--;
+    void merge(vector<int>& n1, int m, vector<int>& n2, int n) {
+        int *a = new int[m+n];
+        int i=0, j=0,k=0;
+        while(i<m && j<n){
+            if(n1[i]<n2[j]){
+                a[k]=n1[i];
+                k++;i++;
             }else{
-                nums1[k]=nums1[i];
-                k--;i--;
+                a[k]=n2[j];
+                k++;j++;
             }
         }
-        while(j>=0){
-            nums1[k]=nums2[j];
-            k--;j--;
+        while(i<m){
+            a[k]=n1[i];
+            k++;i++;
         }
-        while(i>=0){
-            nums1[k] = nums1[i];
-                i--;
-                k--;
+        while(j<n){
+            a[k]=n2[j];
+            k++;j++;
+        }
+        for(int i=0;i<n1.size();i++){
+            n1[i]=a[i];
         }
     }
 };
