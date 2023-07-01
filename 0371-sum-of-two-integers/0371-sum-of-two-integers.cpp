17d0336d1cff92eefb4660d55ad3340085faf5cd
Naman Hiran <106793339+NamanH16@users.noreply.github.com>
2023-07-01 16:57:01 +0530

    Time: 3 ms (43.11%), Space: 5.9 MB (47.46%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        return a+b;
+        if(b==0)return a;
+        int sum=a^b;
+        int carry=(unsigned int)(a&b)<<1;
+        return getSum(sum, carry);
     }
 };
