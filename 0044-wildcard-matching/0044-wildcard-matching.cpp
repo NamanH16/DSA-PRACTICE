ea701b602dcd66cd7905cf0ae344a2fbb1205c4b
Naman Hiran <106793339+NamanH16@users.noreply.github.com>
2023-07-02 18:31:24 +0530

    Time: 55 ms (80.00%), Space: 27.5 MB (38.24%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,35 +1,5 @@
 class Solution {
 public:
-//     int dp[2002][2002];
-    
-//     bool solve(string s, string p, int i,int j){
-//         // base
-//         if(i<0 && j<0)return true;
-//         if(j<0)return false;
-//         if(i<0){
-//             for(int k=0;k<=j;k++){
-//                 if(p[k]!='*') return false;
-//             }
-//             return true;
-//         }
-//         // cache
-//         if(dp[i][j]!=-1)return dp[i][j];
-        
-//         // compute
-//         if(s[i]==p[j] || p[j]=='?'){
-//             return dp[i][j] = solve(s, p, i-1, j-1);
-//         }else if(p[j] == '*'){
-//             return dp[i][j] = (solve(s, p, i, j-1) || solve(s, p, i-1, j));
-//         }
-//             return dp[i][j]= false;   
-//         }
-//         // save n return 
-//     bool isMatch(string s, string p) {
-//         int n = s.length();
-//         int m = p.length();
-//         memset(dp, -1, sizeof(dp));
-//         return solve(s, p, n-1, m-1);
-//     }
      bool solve(int ind1,int ind2,string &s,string &p,vector<vector<int>> &dp){
         if(ind1<0 && ind2<0) return true;
         if(ind2<0) return false;
@@ -46,10 +16,11 @@ public:
         }
         return dp[ind1][ind2]=false;
     }
+    
     bool isMatch(string s, string p) {
-        int n=s.length();
-        int m=p.length();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,s,p,dp);
+        int n = s.length();
+        int m = p.length();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+         return solve(n-1,m-1,s,p,dp);
     }
 };
