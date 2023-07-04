aa696f935676120109d90b091dd4cef88bd4c42c
Naman Hiran <106793339+NamanH16@users.noreply.github.com>
2023-07-04 10:02:37 +0530

    Time: 7 ms (82.86%), Space: 10.1 MB (20.94%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,16 @@
+#define ll long long
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(auto x:nums){
+            mp[x]++;
+        }
+        for(auto i:mp){
+            if(i.second==1){
+                return i.first;
+            }
+        }
+        return -1;
+    }
+};
