287c8be74ce4817454a970e5af596b2c9a9e4755
Naman Hiran <106793339+NamanH16@users.noreply.github.com>
2023-08-10 14:55:25 +0530

    Time: 36 ms (90.88%), Space: 27.7 MB (28.30%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-     bool solve(string &s, string &p, int i,int j,vector<vector<int>> &dp){
-        // base
+    bool solve(string &a,string &b,int i,int j,vector<vector<int>> &dp){
         if(i<0 && j<0)return true;
         if(j<0)return false;
         if(i<0){
             for(int k=0;k<=j;k++){
-                if(p[k]!='*') return false;
+                if(b[k]!='*')return false;
             }
             return true;
         }
-        // cache
         if(dp[i][j]!=-1)return dp[i][j];
-        
-        // compute
-        if(s[i]==p[j] || p[j]=='?'){
-            return dp[i][j] = solve(s, p, i-1, j-1, dp);
-        }else if(p[j] == '*'){
-            return dp[i][j] = (solve(s, p, i, j-1, dp) || solve(s, p, i-1, j, dp));
+        if(a[i]==b[j] || b[j]=='?'){
+            return dp[i][j]=solve(a,b,i-1,j-1,dp);
+        }else if(b[j]=='*'){
+            // * can represent either empty or any string
+            return dp[i][j]=(solve(a,b,i,j-1,dp)||solve(a,b,i-1,j,dp));
+        }else{
+            return dp[i][j]=false;
         }
-            return dp[i][j]= false;   
-        }
-        // save n return 
+    }
+    
     bool isMatch(string s, string p) {
         int n = s.length();
         int m = p.length();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return solve(s, p, n-1, m-1,dp);
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return solve(s,p,n-1,m-1,dp);
     }
 };
