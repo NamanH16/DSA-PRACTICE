730e393e2af7dbaabd0b87397033a791930ee195
Naman Hiran <106793339+NamanH16@users.noreply.github.com>
2023-08-18 23:34:40 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Circular tour/circular-tour.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct petrolPump
+{
+    int petrol;
+    int distance;
+};
+
+
+// } Driver Code Ends
+/*
+The structure of petrolPump is 
+struct petrolPump
+{
+    int petrol;
+    int distance;
+};*/
+
+/*You are required to complete this method*/
+class Solution{
+  public:
+  
+    //Function to find starting point where the truck can start to get through
+    //the complete circle without exhausting its petrol in between.
+    int tour(petrolPump p[],int n)
+    {
+       //Your code here
+       int requiredFuel = 0, extraFuel = 0;
+       int ans = 0;
+       for(int i=0;i<n;i++){
+           extraFuel+=(p[i].petrol - p[i].distance);
+           if(extraFuel<0){
+               ans=i+1;
+               requiredFuel+=extraFuel;
+               extraFuel=0;
+           }
+       }
+       if(extraFuel+requiredFuel>=0){
+           return ans;
+       }
+       return -1;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        petrolPump p[n];
+        for(int i=0;i<n;i++)
+            cin>>p[i].petrol>>p[i].distance;
+        Solution obj;
+        cout<<obj.tour(p,n)<<endl;
+    }
+}
+
+// } Driver Code Ends
